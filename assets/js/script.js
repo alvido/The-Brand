d41ef704bc14b078c981b683276d21b2637350a9
alvido <al.dorobalo@gmail.com>
2023-03-20 23:42:19 +0200

    working on the main page

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$("#search_btn").click(function() {
+        $('#search').toggleClass('active');
+    });
+
+$("#burder").click(function() {
+        $('.header_nav').toggleClass('active');
+    });
+$(".close").click(function() {
+        $('.header_nav').removeClass('active');
+    });
+
