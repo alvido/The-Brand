e734378651ffb80a68f39143a47cbf0035b7b031
alvido <al.dorobalo@gmail.com>
2023-04-12 20:09:28 +0300

    change filter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,30 @@
-$("#search_btn").click(function() {
-        $('#search').toggleClass('active');
-    });
+//
+$(".search_btn").click(function() {
+    $('.search').toggleClass('active');
+});
+//
 
-$("#burder").click(function() {
-        $('.header_nav').toggleClass('active');
-    });
+//
+$(".burder").click(function() {
+    $('.header_nav').toggleClass('active');
+});
+//
+
+//
 $(".close").click(function() {
-        $('.header_nav').removeClass('active');
-    });
+    $('.header_nav').removeClass('active');
+});
+//
+
+//
+$(".filter_text").click(function() {
+    $(this).parent().toggleClass('active');
+});
+//
+
+//
+$(".filter_title").click(function() {
+    $(this).parent().toggleClass('active');
+});
+//
 
