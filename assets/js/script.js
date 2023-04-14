9ebae7660a7ab2f57c1532171d786dd566b03b41
alvido <al.dorobalo@gmail.com>
2023-04-14 18:18:30 +0300

    media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,51 @@
-//
+// open and close search input
 $(".search_btn").click(function() {
-    $('.search').toggleClass('active');
+    $('#search').toggleClass('active');
 });
 //
 
-//
-$(".burder").click(function() {
+// open header nav
+$("#burder").click(function() {
     $('.header_nav').toggleClass('active');
 });
 //
 
-//
+// close header nav
 $(".close").click(function() {
     $('.header_nav').removeClass('active');
 });
 //
 
 //
+$(document).mouseup(function (e){  
+    var div = $(".header_nav");  //класс элемента вне которого клик
+    if (!div.is(e.target) && div.has(e.target).length === 0) {  
+            div.removeClass('active');  
+    }
+  });
+//
+
+// open and close filter
+$(".filter_title").click(function() {
+    $('.filter_title').not(this).removeClass('active');
+    $(this).toggleClass('active');
+    $('.filter_title').not(this).parent().removeClass('active');
+    $(this).parent().toggleClass('active');
+    $('.filter_title').not(this).next().removeClass('active');
+    $(this).next().toggleClass('active');
+});
+//
+
+// opeb and close sublist in filter
 $(".filter_text").click(function() {
+    $(this).parent().siblings().removeClass('active');
     $(this).parent().toggleClass('active');
 });
 //
 
-//
-$(".filter_title").click(function() {
-    $(this).parent().toggleClass('active');
+// link product html
+$(".product_item").click(function() {
+    window.location.href = "product.html";
 });
 //
 
